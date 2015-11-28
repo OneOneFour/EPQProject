3060f34f717fecc4f3544e6206b870e10949a629
OneOneFour <robertkng104@gmail.com>
2015-11-28 15:48:13 +0000

    Updated anglular functions to use doubles instead of floats

--- EPQProject/Helpies.cpp
+++ EPQProject/Helpies.cpp
@@ -1,6 +1,5 @@
 #define _USE_MATH_DEFINES
 #include "Helpies.hpp"
-
 float coloumbsConstant = 9000000000;
 
 float genRandomFloat(float min, float max){
@@ -13,10 +12,10 @@ float getMagnitude(sf::Vector2f A) {
 float getDistance(sf::Vector2f A, sf::Vector2f B){
 	return std::sqrt(std::pow(A.x - B.x, 2) + std::pow(A.y - B.y, 2));
 }
-float getArgument(sf::Vector2f A){
+double getArgument(sf::Vector2f A){
 	return std::atan2(A.y, A.x);
 }
-float getArgumentH(sf::Vector2f A){
+double getArgumentH(sf::Vector2f A){
 	return std::atan2(A.x, A.y);
 }
 void normalizeVector(sf::Vector2f& vector){
@@ -27,10 +26,10 @@ void normalizeVector(sf::Vector2f& vector){
 sf::Vector2f lerp(sf::Vector2f vectorA, sf::Vector2f vectorB, float lerpFactor){
 	return sf::Vector2f(vectorA.x + (lerpFactor* (vectorB.x - vectorA.x)), vectorA.y + (lerpFactor* (vectorB.y - vectorA.y)));
 }
-float degreeToRad(float angle){
-	return (angle / 180)* M_PI;
+double degreeToRad(float angle){
+	return (angle / 180.0)* M_PI;
 	
 }
-float radToDegree(float angle){
-	return (angle / M_PI) * 180;
+double radToDegree(float angle){
+	double (angle / M_PI) * 180.0;
 }
